45855e157833552ed84740a327a34756bc19b638
snowynaram <joe9032@naver.com>
2024-01-21 06:19:02 +0900

    array

--- js_first/potato.js
+++ js_first/potato.js
@@ -1,3 +1,15 @@
-const amIFat = true;
-let something;
-console. log(something);
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sun";
+
+
+const daysOfWeek = [mon, tue, wed, thu, fri, sat];
+
+console. log (daysOfWeek[5]);
+
+daysOfWeek. push(sun);
+console. log (daysOfWeek);
