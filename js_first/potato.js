8b383e61c65c66fc746cf1b06e44bed54da8de6b
snowynaram <joe9032@naver.com>
2024-01-21 05:58:20 +0900

    null&undefined

--- js_first/potato.js
+++ js_first/potato.js
@@ -1,11 +1,3 @@
-let a = 6;
-const b = 3;
-let myName = "minseok";
-
-console. log(a+b);
-console. log(a*b);
-console. log(a/b);
-console. log("my name is "+myName);
-
-myName = "민석";
-console. log("내이름은 "+myName);
+const amIFat = true;
+let something;
+console. log(something);
