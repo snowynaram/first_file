1e59908165907cd443a63743918c3a4b7ecb1f0a
snowynaram <joe9032@naver.com>
2024-01-27 23:47:39 +0900

    querySelector

--- js_first/potato.js
+++ js_first/potato.js
@@ -1,6 +1,6 @@
-const title = document.getElementById("title");
+//hello안에 h1을 찾아서 알려줌
+//querySelector은 제일 처음것만 가져와줌 모두가져오고싶을때는 뒤에 All을 붙여주면 된다.
+//querySelector는 매우 중요@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+const title = document.querySelector("div h1");
 
-title.innerText = "Got you bitch"
-
-console.log(title.id);
-console.log(title.className);
+console.log(title);
