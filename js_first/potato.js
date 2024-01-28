466e218b76b4ffeec18bf4b96763c028c04fbd25
snowynaram <joe9032@naver.com>
2024-01-29 00:21:31 +0900

    event funny haha

--- js_first/potato.js
+++ js_first/potato.js
@@ -1,20 +1,45 @@
 
-const title = document.querySelector("div.hello:first-child h1");
+const h1 = document.querySelector("div.hello:first-child h1");
 
-console.dir(title);
+console.dir(h1);
+h1.style.color = "blue";
+
 function handleTitleClick(){
-    title.style.color = "blue";
+    h1.style.color = "black";
 }
 
 function mouseenter(){
-    title.innerText = "mouse is here";
+    h1.innerText = "마우스 여깄당.";
+    h1.style.color = "blue";
 }
 
 function mouseleave(){
-    title.innerText = "Mouse is gone!";
-    title.style.color = "black";
+    h1.innerText = "Mouse is gone!";
+    h1.style.color = "red";
 }
 
-title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter", mouseenter);
-title.addEventListener("mouseleave", mouseleave);
+function handleresize(){
+    document.body.style.backgroundColor = "black";
+}
+
+function windowCopy(){
+    alert ("복사하지마!");
+}
+
+function windowoffline(){
+    alert ("인터넷이 끊겼어요!!");
+}
+
+function windowonline(){
+    alert ("인터넷이 돌아왔다.");
+}
+
+h1.addEventListener("click", handleTitleClick);
+h1.addEventListener("mouseenter", mouseenter);
+h1.addEventListener("mouseleave", mouseleave);
+
+
+window.addEventListener("resize", handleresize);
+window.addEventListener("copy", windowCopy);
+window.addEventListener("offline",windowoffline);
+window.addEventListener("online",windowonline);
