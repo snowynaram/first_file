348f1bceba9741f9e72b78b430d810e65e9ff8f4
snowynaram <joe9032@naver.com>
2024-01-29 00:00:37 +0900

    event

--- js_first/potato.js
+++ js_first/potato.js
@@ -1,11 +1,20 @@
-//html을 가져오고
+
 const title = document.querySelector("div.hello:first-child h1");
 
-//클릭햇을때 실행하는것
-//색을 blue로 바꿈
+console.dir(title);
 function handleTitleClick(){
     title.style.color = "blue";
 }
-//addEventListener무슨 이벤트를 "click"클릭이벤트를 handleTitleClick작동시킨다.
-title.addEventListener("click", handleTitleClick);
 
+function mouseenter(){
+    title.innerText = "mouse is here";
+}
+
+function mouseleave(){
+    title.innerText = "Mouse is gone!";
+    title.style.color = "black";
+}
+
+title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", mouseenter);
+title.addEventListener("mouseleave", mouseleave);
