7e4c14ea981d2d40d1bec68d3b53e7d187e57b85
snowynaram <joe9032@naver.com>
2024-01-31 22:26:48 +0900

    preventDefault

--- js_first/potato.js
+++ js_first/potato.js
@@ -1,14 +1,10 @@
+const loginForm = document.querySelector("#login-form");
 const logininput = document.querySelector("#login-form input");
-const loginbutton = document.querySelector("#login-form button");
 
-function onLoginBtnClick(){
-   const name = logininput.value;
-    if(name === ""){
-alert ("이름을 입력하세요");
-    }else{
-        alert ("안녕 "+ logininput.value);
-    }
+function onLoginSubmit(event){
+    event. preventDefault();
+    console.log(logininput.value);
 }
 
 
-loginbutton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
