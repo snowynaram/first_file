00fd9f00b30c00ef805fbe5b9cca0698af59269e
snowynaram <joe9032@naver.com>
2024-02-01 23:00:03 +0900

    사이트주소넣기

--- js_first/potato.js
+++ js_first/potato.js
@@ -1,10 +1,19 @@
 const loginForm = document.querySelector("#login-form");
 const logininput = document.querySelector("#login-form input");
 
+const link= document.querySelector("a");
+
+
 function onLoginSubmit(event){
     event. preventDefault();
     console.log(logininput.value);
 }
-
+function handleLinkClick(event) {
+    event.preventDefault()
+    console.dir(event);
+    alert("사이트로 이동합니다!");
+}
 
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click",handleLinkClick);
+
