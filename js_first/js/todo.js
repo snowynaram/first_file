a2f7324af824f7c1ebc0d3543fe3ca2f074fe59f
snowynaram <joe9032@naver.com>
2024-02-15 23:39:56 +0900

    todo2

--- js_first/js/todo.js
+++ js_first/js/todo.js
@@ -2,10 +2,28 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteTodo(event){
+    const li = event.target.parentElement;
+    li.remove();
+}
+
+function paintToDo(newToDo){
+ const li = document.createElement("li");
+ const span = document.createElement("span");
+ span.innerText = newToDo;
+ const button = document.createElement("button");
+ button.innerText = "X";
+ button.addEventListener("click", deleteTodo);
+ li.appendChild(span);
+ li.appendChild(button);
+ toDoList.appendChild(li);
+}
+
 function handleToDoSubmit(event){
     event.preventDefault();
     const newToDo = toDoInput.value;
     toDoInput.value = "";
+    paintToDo(newToDo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit)
