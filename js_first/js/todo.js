4f75f0548c865cd38f62124fd3068f7b3a65fe7a
snowynaram <joe9032@naver.com>
2024-02-16 23:27:56 +0900

    todo3

--- js_first/js/todo.js
+++ js_first/js/todo.js
@@ -2,6 +2,12 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];
+
+function saveToDos(){
+    localStorage.setItem("todos", JSON.stringify(toDos));
+}
+
 function deleteTodo(event){
     const li = event.target.parentElement;
     li.remove();
@@ -23,7 +29,9 @@ function handleToDoSubmit(event){
     event.preventDefault();
     const newToDo = toDoInput.value;
     toDoInput.value = "";
+    toDos.push(newToDo);
     paintToDo(newToDo);
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit)
