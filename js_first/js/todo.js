5ac0e362d1c8df17a8dd5aa43c6ee6f102ccd3c4
snowynaram <joe9032@naver.com>
2024-02-20 21:56:15 +0900

    todo fin

--- js_first/js/todo.js
+++ js_first/js/todo.js
@@ -11,12 +11,15 @@ function saveToDos(){
 function deleteTodo(event){
     const li = event.target.parentElement;
     li.remove();
+    toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+    saveToDos();
 }
 
 function paintToDo(newToDo){
  const li = document.createElement("li");
+ li.id = newToDo.id;
  const span = document.createElement("span");
- span.innerText = newToDo;
+ span.innerText = newToDo.text;
  const button = document.createElement("button");
  button.innerText = "X";
  button.addEventListener("click", deleteTodo);
@@ -29,8 +32,12 @@ function handleToDoSubmit(event){
     event.preventDefault();
     const newToDo = toDoInput.value;
     toDoInput.value = "";
-    toDos.push(newToDo);
-    paintToDo(newToDo);
+    const newTodoObj = {
+        text : newToDo,
+        id : Date.now()
+    }
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
     saveToDos();
 }
 
@@ -38,8 +45,8 @@ toDoForm.addEventListener("submit", handleToDoSubmit)
 
 const savedToDos = localStorage.getItem("todos")
 
-if(savedToDos){
+if(savedToDos !== null){
     const parsedToDos = JSON.parse(savedToDos);
     toDos = parsedToDos;
-    parsedToDos.forEach(paintToDo);
+    parsedToDos.forEach(paintToDo); 
     };   
