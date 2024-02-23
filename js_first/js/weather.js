a33d6c5818808fe62c78d642289871aae8351708
snowynaram <joe9032@naver.com>
2024-02-24 00:01:38 +0900

    weather API

--- js_first/js/weather.js
+++ js_first/js/weather.js
@@ -1,7 +1,19 @@
+
+const API_KEY = "4889561e302f8d4489c0802a70067d07"
+
 function onGeoOk(position){
 const lat = position.coords.latitude;
 const lng = position.coords.longitude;
-console.log ("당신은", lat, lng);
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+fetch(url)
+    .then(response => response.json())
+    .then(data =>{
+        const weather = document.querySelector("#weather span:first-child");
+        const location = document.querySelector("#weather span:last-child");
+        location.innerText = data.name;
+        weather.innerText = data.weather[0].main;
+});
+
 }
 function onGeoErr(){
     alert("날씨를 찾을수가 없어요 ㅠ");
